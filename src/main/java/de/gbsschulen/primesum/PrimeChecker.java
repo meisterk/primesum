80b20151cbb0d172edd74382897c79444222b2f4
meisterk <franz@kohnlehome.de>
2020-02-05 13:52:02 +0100

    Add PrimeCheckerTest

--- src/main/java/de/gbsschulen/primesum/PrimeChecker.java
+++ src/main/java/de/gbsschulen/primesum/PrimeChecker.java
@@ -1,14 +1,14 @@
 package de.gbsschulen.primesum;
 
-public class PrimeChecker implements IPrimeChecker{
+public class PrimeChecker implements IPrimeChecker {
     @Override
     public boolean isPrime(int number) {
-        if(number<2){
+        if (number < 2) {
             return false;
         }
 
         for (int i = 2; i < number; i++) {
-            if(number%i == 0){
+            if (number % i == 0) {
                 return false;
             }
         }
